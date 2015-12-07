b1443c40ed9939f4202383f81f60e6492de1ff54
Enrique Gonzalez <enriikke@gmail.com>
2015-12-07 07:31:09 -0500

    Added confirmable back.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :groups
 
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end
