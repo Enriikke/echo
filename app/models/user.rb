1e0ecd6c16ee8e370f911b4a12e45de9120fb0f6
Enrique Gonzalez <enriikke@gmail.com>
2015-12-07 07:08:15 -0500

    Temporarily removed confirmation from user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :groups
 
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
